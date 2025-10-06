30046a23a369cacd93ce5d8e1fcfb646eb8097ae
thun888 <2238342947@qq.com>
2025-10-06 16:37:12 +0800

    add tags

---
+++ scripts/tags/lib/hide.js
@@ -0,0 +1,14 @@
+/**
+ * hide.js v1.1 | https://github.com/weekdaycare/hexo-theme-stellar/
+ * 格式与官方标签插件一致，使用空格分隔；中括号内的是可选参数（中括号不需要写出来）
+ *
+ * {% hide %}{% endhide %}
+ */
+
+'use strict'
+
+module.exports = (ctx) => {
+  return function (args) {
+    return "";
+  }
+}
