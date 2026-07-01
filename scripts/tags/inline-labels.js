920f688ff679468600c4d346da18b022360c40ba
thun888 <thun888@hzchu.top>
2026-07-01 13:08:57 +0800

    feat(kbd): add loopspeed param for key-press loop animation

--- scripts/tags/inline-labels.js
+++ scripts/tags/inline-labels.js
@@ -21,7 +21,22 @@ hexo.extend.tag.register('del', function(args) {
   return `<del>${args.join(' ')}</del>`
 })
 hexo.extend.tag.register('kbd', function(args) {
-  return `<kbd>${args.join(' ')}</kbd>`
+  args = hexo.args.map(args, ['loopspeed'], ['text'])
+  var el = ''
+  el += '<kbd class="tag-plugin kbd"'
+  if (args.loopspeed && args.loopspeed !== '0') {
+    // 动画速度，默认 2s；支持简写如 "2s"、"500ms"、"3"
+    var speed = args.loopspeed
+    if (/^\d+$/.test(speed)) {
+      speed = speed + 's'
+    }
+    el += ` loopspeed="${args.loopspeed}"`
+    el += ` style="animation-duration:${speed}"`
+  }
+  el += '>'
+  el += args.text || args._ || ''
+  el += '</kbd>'
+  return el
 })
 hexo.extend.tag.register('psw', function(args) {
   return `<psw>${args.join(' ')}</psw>`
